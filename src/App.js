50688ef89fba0234e1faf3b52de2908f0340874a
jbilee <julishahn@gmail.com>
2023-11-10 14:45:22 +0900

    feat: 날짜 입력 기능 구현 - 사용자로부터 방문 예정 날짜를 입력 받는 기능 추가 - 유효하지 않은 입력값에 대한 예외 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputView from './InputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    const reservationDate = await this.getReservationDate();
+  }
+
+  async getReservationDate() {
+    let input;
+
+    do {
+      try {
+        input = await InputView.readDate();
+        this.validateDate(input);
+      } catch (error) {
+        Console.print(error.message);
+        input = null;
+      }
+    } while (!input);
+
+    return input;
+  }
+
+  validateDate(inputString) {
+    const inputNumber = Number(inputString);
+    if (inputNumber < 1 || inputNumber > 31 || inputString.includes('.'))
+      throw new Error('[ERROR] 유효한 날짜(1~31)가 아닙니다.');
+    if (Number.isNaN(inputNumber))
+      throw new Error('[ERROR] 숫자만 입력해 주세요.');
+  }
 }
 
 export default App;
