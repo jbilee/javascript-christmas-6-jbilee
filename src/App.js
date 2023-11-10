fdbefd296c97ea7dc8988ede6846645886920a9b
jbilee <julishahn@gmail.com>
2023-11-10 17:07:07 +0900

    feat: 주문 입력 기능 구현 - 사용자로부터 주문할 메뉴를 입력 받는 기능 추가 - 유효하지 않은 입력값에 대한 예외 처리 구현
    fix($validateDate): 날짜 예외 처리에 대한 에러 문구 수정
    - 날짜 예외 처리 시 출력하는 에러 문구를 미션 요구 사항에 지정된 문구로 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputView from './InputView.js';
+import { RESTAURANT_MENU } from './constants.js';
 
 class App {
   async run() {
     const reservationDate = await this.getReservationDate();
+    const order = await this.getOrder();
   }
 
   async getReservationDate() {
@@ -22,12 +24,67 @@ class App {
     return input;
   }
 
+  async getOrder() {
+    let input;
+
+    do {
+      try {
+        input = await InputView.readMenu();
+        this.validateOrder(input);
+      } catch (error) {
+        Console.print(error.message);
+        input = null;
+      }
+    } while (!input);
+
+    return input;
+  }
+
   validateDate(inputString) {
     const inputNumber = Number(inputString);
-    if (inputNumber < 1 || inputNumber > 31 || inputString.includes('.'))
-      throw new Error('[ERROR] 유효한 날짜(1~31)가 아닙니다.');
-    if (Number.isNaN(inputNumber))
-      throw new Error('[ERROR] 숫자만 입력해 주세요.');
+    if (
+      inputNumber < 1 ||
+      inputNumber > 31 ||
+      inputString.includes('.') ||
+      Number.isNaN(inputNumber)
+    )
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+  }
+
+  validateOrder(inputString) {
+    const order = {};
+
+    inputString.split(',').forEach((input) => {
+      const itemOrdered = input.split('-');
+      const [menuName, itemCount] = itemOrdered;
+
+      if (
+        !RESTAURANT_MENU[menuName] ||
+        Number.isNaN(Number(itemCount)) ||
+        itemCount.includes('.') ||
+        Number(itemCount) < 1
+      )
+        throw new Error(
+          '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+        );
+
+      order[menuName] = itemCount;
+    });
+
+    const totalItemCount = Object.values(order).reduce((acc, num) => {
+      return acc + Number(num);
+    }, 0);
+
+    if (totalItemCount > 20)
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+
+    const itemCategories = Object.keys(order).map(
+      (item) => RESTAURANT_MENU[item].CATEGORY,
+    );
+    const itemSet = new Set(itemCategories);
+
+    if (itemSet.size === 1 && itemSet.has('drinks'))
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
   }
 }
 
---
+++ src/constants.js
@@ -0,0 +1,50 @@
+export const RESTAURANT_MENU = {
+  양송이수프: {
+    CATEGORY: 'appetizers',
+    PRICE: 6000,
+  },
+  타파스: {
+    CATEGORY: 'appetizers',
+    PRICE: 5500,
+  },
+  시저샐러드: {
+    CATEGORY: 'appetizers',
+    PRICE: 8000,
+  },
+  티본스테이크: {
+    CATEGORY: 'main',
+    PRICE: 55000,
+  },
+  바비큐립: {
+    CATEGORY: 'main',
+    PRICE: 54000,
+  },
+  해산물파스타: {
+    CATEGORY: 'main',
+    PRICE: 35000,
+  },
+  크리스마스파스타: {
+    CATEGORY: 'main',
+    PRICE: 25000,
+  },
+  초코케이크: {
+    CATEGORY: 'desserts',
+    PRICE: 15000,
+  },
+  아이스크림: {
+    CATEGORY: 'desserts',
+    PRICE: 5000,
+  },
+  제로콜라: {
+    CATEGORY: 'drinks',
+    PRICE: 3000,
+  },
+  레드와인: {
+    CATEGORY: 'drinks',
+    PRICE: 60000,
+  },
+  샴페인: {
+    CATEGORY: 'drinks',
+    PRICE: 25000,
+  },
+};
