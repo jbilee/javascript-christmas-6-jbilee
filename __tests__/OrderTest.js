462d8ad16e55ee15b0708dbbf86510b33a74cea9
jbilee <julishahn@gmail.com>
2023-11-14 18:20:59 +0900

    test($Order): Order 클래스에 대한 테스트 코드 작성 - 이벤트 혜택을 받을 수 있는 기준 금액 테스트 - 주문이 가능한 최소 조건 테스트

---
+++ __tests__/OrderTest.js
@@ -0,0 +1,26 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Order from '../src/Order.js';
+
+describe('Order 클래스 테스트', () => {
+  test('주문 총액이 만원 미만일 경우 할인 혜택 없음', () => {
+    const TEST_DATE = '1';
+    const TEST_ORDER = '양송이수프-1';
+    const EXPECTED_DISCOUNT = 0;
+
+    const newOrder = new Order(TEST_DATE, TEST_ORDER);
+    const baseTotal = newOrder.calculateBaseTotal();
+    const summary = newOrder.getDiscountSummary(baseTotal);
+    const discount = newOrder.calculateTotalDiscount(summary);
+
+    expect(discount).toBe(EXPECTED_DISCOUNT);
+  });
+
+  test('음료만 주문할 경우 예외 처리', () => {
+    const TEST_DATE = '1';
+    const TEST_ORDER = '레드와인-10';
+
+    expect(() => {
+      new Order(TEST_DATE, TEST_ORDER);
+    }).toThrow('[ERROR]');
+  });
+});
