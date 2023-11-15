cd16d427dfc6fd291c9049763b3b3bdd42b76dda
jbilee <julishahn@gmail.com>
2023-11-15 11:42:32 +0900

    test($PromotionTest): Promotion 클래스 테스트 추가 - 평일, 주말, 디데이 이벤트마다 총 혜택 금액을 계산하는 기능 테스트

--- __tests__/PromotionTest.js
+++ __tests__/PromotionTest.js
@@ -20,6 +20,60 @@ describe('Promotion 클래스 테스트', () => {
     }
   });
 
+  test('평일 혜택 금액 계산 기능 테스트', () => {
+    const TEST_DATES = [4, 5, 6];
+    const TEST_CATEGORIES = [
+      ['appetizers', 'desserts', 'drinks'],
+      ['main', 'desserts', 'desserts'],
+      ['appetizers', 'main', 'drinks'],
+    ];
+    const PROMOTION_TYPE = 'weekdays';
+    const expected = [2023, 4046, 0];
+
+    for (let i = 0; i < TEST_DATES.length; i += 1) {
+      const newPromotion = new Promotion(TEST_DATES[i]);
+      expect(
+        newPromotion.calculateDiscounts(PROMOTION_TYPE, TEST_CATEGORIES[i]),
+      ).toBe(expected[i]);
+    }
+  });
+
+  test('주말 혜택 금액 계산 기능 테스트', () => {
+    const TEST_DATES = [1, 2, 8];
+    const TEST_CATEGORIES = [
+      ['appetizers', 'main', 'drinks'],
+      ['main', 'main', 'desserts'],
+      ['appetizers', 'desserts', 'drinks'],
+    ];
+    const PROMOTION_TYPE = 'weekends';
+    const expected = [2023, 4046, 0];
+
+    for (let i = 0; i < TEST_DATES.length; i += 1) {
+      const newPromotion = new Promotion(TEST_DATES[i]);
+      expect(
+        newPromotion.calculateDiscounts(PROMOTION_TYPE, TEST_CATEGORIES[i]),
+      ).toBe(expected[i]);
+    }
+  });
+
+  test('디데이 혜택 금액 계산 기능 테스트', () => {
+    const TEST_DATES = [1, 10, 25];
+    const TEST_CATEGORIES = [
+      ['appetizers', 'main', 'drinks'],
+      ['main', 'main', 'desserts'],
+      ['appetizers', 'desserts', 'drinks'],
+    ];
+    const PROMOTION_TYPE = 'dDaySales';
+    const expected = [1000, 1900, 3400];
+
+    for (let i = 0; i < TEST_DATES.length; i += 1) {
+      const newPromotion = new Promotion(TEST_DATES[i]);
+      expect(
+        newPromotion.calculateDiscounts(PROMOTION_TYPE, TEST_CATEGORIES[i]),
+      ).toBe(expected[i]);
+    }
+  });
+
   test('입력한 주문의 모든 혜택 금액 추출 기능 테스트', () => {
     const TEST_DATES = [1, 3, 5, 26, 29, 31];
     const TEST_CATEGORIES = ['main', 'desserts', 'desserts'];
